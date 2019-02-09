68427422f49c2db0ec3272de640d044502a3cb84
Alfonso Herreros <aherreros@atsistemas.com>
2019-02-09 21:07:25 +0100

    caga inicial casi

--- configServer/src/main/java/com/buongiorno/cat/microservices/configServer/ConfigServerApp.java
+++ configServer/src/main/java/com/buongiorno/cat/microservices/configServer/ConfigServerApp.java
@@ -2,11 +2,12 @@ package com.buongiorno.cat.microservices.configServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;       
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApp {
+public class ConfigServerApp extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApp.class, args);
