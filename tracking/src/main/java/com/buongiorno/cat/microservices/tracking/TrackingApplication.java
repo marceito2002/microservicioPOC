683305173f28cbe564f0788b1638a40d804561dc
alfonso.herreros <aherreros@atsistemas.com>
2019-02-13 12:55:39 +0100

    inicial

--- tracking/src/main/java/com/buongiorno/cat/microservices/tracking/TrackingApplication.java
+++ tracking/src/main/java/com/buongiorno/cat/microservices/tracking/TrackingApplication.java
@@ -2,10 +2,10 @@ package com.buongiorno.cat.microservices.tracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class TrackingApplication {
 
