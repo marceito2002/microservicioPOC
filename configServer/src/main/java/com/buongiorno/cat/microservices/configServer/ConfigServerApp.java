359237f762efffe88de32b6b0433a531a449974d
alfonso.herreros <aherreros@atsistemas.com>
2019-02-14 15:47:58 +0100

    carga inicial con docker

--- configServer/src/main/java/com/buongiorno/cat/microservices/configServer/ConfigServerApp.java
+++ configServer/src/main/java/com/buongiorno/cat/microservices/configServer/ConfigServerApp.java
@@ -1,13 +1,12 @@
 package com.buongiorno.cat.microservices.configServer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;       
+import org.springframework.boot.autoconfigure.SpringBootApplication;     
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApp extends SpringBootServletInitializer {
+public class ConfigServerApp  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApp.class, args);
